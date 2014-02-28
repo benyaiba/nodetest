6507a93905be8562c74e6180b41554cb90aa61d0
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2014-02-28 09:19:25 +0800

    add datapicker test

--- express/public/index.js
+++ express/public/index.js
@@ -1,14 +1,14 @@
 $(function() {
-	console.log("ready ...");
-        // init datatable
-	initDataTable();
-	// init newBtn for bootstrap dialog
-	$("#newBtn").on("click", function(){
-		$("#testDialog").modal("show");
-		$("#testDialog .modal-footer .btn").off("click").on("click", function(){
-			$("#testDialog").modal("hide");
-		});
-	});
+  console.log("ready ...");
+  // init datatable
+  initDataTable();
+  // init newBtn for bootstrap dialog
+  $("#newBtn").on("click", function(){
+    $("#testDialog").modal("show");
+    $("#testDialog .modal-footer .btn").off("click").on("click", function(){
+      $("#testDialog").modal("hide");
+    });
+  });
   // init group button
   $(".btn-group").delegate("a", "click", function(){
     console.log($(this).html());
@@ -20,50 +20,52 @@ $(function() {
   $("#testLink2").off("click").on("click", function(){
     return false;
   });
-	// init dialog
-	$("#td").dialog({autoOpen: false, buttons: [{"text": "close", click: function(){$(this).dialog("close")}}]});
-	$("#openDialogBtn").on("click", function(){
-		$("#td").dialog("open");
-	});
+  // init dialog
+  $("#td").dialog({autoOpen: false, buttons: [{"text": "close", click: function(){$(this).dialog("close")}}]});
+  $("#openDialogBtn").on("click", function(){
+    $("#td").dialog("open");
+  });
+  // init datepicker
+  $("#picker").datepicker();
 });
 
 function initDataTable() {
-	$("#example").dataTable({
-		"bPaginate": false
-			,
-		"aoColumns": [{
-			"sTitle": "Site",
-			"mData": "site"
-		},
-		{
-			"sTitle": "Id",
-			"mData": "id"
-		},
-		{
-			"sTitle": "Password",
-			"mData": "password"
-		}]
-	});
-	initAjax();
+  $("#example").dataTable({
+    "bPaginate": false
+      ,
+    "aoColumns": [{
+      "sTitle": "Site",
+      "mData": "site"
+    },
+    {
+      "sTitle": "Id",
+      "mData": "id"
+    },
+    {
+      "sTitle": "Password",
+      "mData": "password"
+    }]
+  });
+  initAjax();
 
 }
 
 function initAjax(){
-	$.ajax({
-		url: "/user/zhao_hongsheng",
-		type: "GET",
-		dataType: "json",
-		success: function(json) {
-			console.log("success...", json);
-			var dtObj = $("#example").dataTable();
-			dtObj.fnClearTable();
-			dtObj.fnAddData(json);
-		},
-		error: function(xhr, status) {
-			console.log("error", arguments);
-		},
-		complete: function(xhr, status) {
-			console.log("complete ...");
-		}
-	});
+  $.ajax({
+    url: "/user/zhao_hongsheng",
+    type: "GET",
+    dataType: "json",
+    success: function(json) {
+      console.log("success...", json);
+      var dtObj = $("#example").dataTable();
+      dtObj.fnClearTable();
+      dtObj.fnAddData(json);
+    },
+    error: function(xhr, status) {
+      console.log("error", arguments);
+    },
+    complete: function(xhr, status) {
+      console.log("complete ...");
+    }
+  });
 }
