3473825862e331f7c2f634d0d1b10fd33d9e5fdc
zhao_hongsheng <yisurenali@aliyun.com>
2014-06-21 20:27:45 +0800

    add datepicker test [include multiple select datepicker]

---
+++ express/public/js/app/testDatePicker.js
@@ -0,0 +1,22 @@
+function testDatePickerInit(){
+	$("#dp1").datepicker();
+	
+//	$("#dp2").multiDatesPicker({
+//		dateFormat: "yy-mm-dd"
+//	});
+
+	$("#dp3").datepick({
+		dateFormat: "yyyy-mm-dd",
+		multiSelect:100
+	});
+    $("#btn1").datepick({
+        dateFormat: "yyyy-mm-dd",
+        multiSelect: 100,
+        multiSeparator: " ",
+        altField: $("#dateOutput"),
+        onSelect: function(){
+            $("#btn1").val("select ...");
+            $("#dateOutput").html($("#dateOutput").val());
+        }
+    });
+}
