3397a8b5208b2e25e6579371ccd184a42862976a
yisuren <zhao_hongsheng@microad-tech.com>
2015-01-06 13:55:43 +0800

    finish net2.js Dispatcher & Action

--- socket/chatroom/net2.js
+++ socket/chatroom/net2.js
@@ -1,9 +1,16 @@
 var net = require("net");
 var sockets = [];
 
-
-
 var server = net.createServer(function(socket) {
+
+    // 每个socket一个action，
+    // action和socket相互依赖（相互依赖不好？）
+    var action = new Action();
+    action.setSocket(socket);
+    socket.action = action;
+
+    // 显示欢迎信息
+    socket.action.welcome();
 
     var message = ""
     socket.on("data", function(data) {
@@ -11,134 +18,25 @@ var server = net.createServer(function(socket) {
         if (!/\r\n/.test(input)) {
             message += input;
         } else {
-            // get one line input
-            
+            // 读取到一行消息
             message = message.trim();
-            if (status < STATUS.name_ok) {
-                // need set user name, first
-            } else {
-                // run command or broadcast chat
-            }
+            Dispatcher.dispatch(socket, message);
             message = "";
-        }
-        
-
-        
-        function execCommand(message){
-            status = STATUS.name_ok;
-            if (/^\\who$/.test(message)) {
-                status = STATUS.command_who;
-            }
-            if (/^\\help$/.test(message)) {
-                status = STATUS.command_help;
-            }
-            if (/^\\change_name\s.*$/.test(message)) {
-                status = STATUS.command_change_name;
-            }
-            if (/^@\w+\s.*$/.test(message)) {
-                status = STATUS.command_private;
-            }
-
-            switch (status) {
-            case STATUS.command_help:
-                socket.write("# \\who to check one line users\r\n");
-                socket.write("# \\change_name mike, to change your name to mike\r\n");
-                socket.write("# @mike xxx , to chat mike only\r\n");
-                socket.write("# \\help to show this list\r\n");
-                break;
-            case STATUS.command_who:
-                var names = getUserNameList();
-                for (var i = 0; i < names.length; i++) {
-                    socket.write("# " + names[i] + "\r\n");
-                }
-                break;
-            case STATUS.command_change_name:
-                var newName = message.replace("\\change_name ", "");
-                var oldName = socket.userInfo.name;
-                socket.userInfo.name = newName;
-                socket.write("# change your name to [" + newName + "]\r\n");
-                broadcast("# [" + oldName + "] change name to [" + newName + "]");
-                break;
-            case STATUS.command_private:
-                var fromName = socket.userInfo.name;
-                var privateMsg = message.split(" ")[1];
-                var toName = message.split(" ")[0].replace("@", "");
-                var toSocket = findSocketByName(toName);
-                if(!toSocket){
-                    socket.write("# sorry , user not found .\r\n");
-                }else{
-                    toSocket.write("* [" + fromName + "] say to you: " + privateMsg + "\r\n");
-                }
-                break;
-            default:
-                broadcast(message, socket);
-            }
         }
     });
 
     socket.on("end", function() {
-        removeOneSocket(socket);
+        socket.action.leave();
     });
-
-    function findSocketByName(name) {
-        for (var i = 0; i < sockets.length; i++) {
-            var s = sockets[i];
-            if (s.userInfo.name == name) {
-                return s;
-            }
-        }
-    }
-
-    function getUserNameList() {
-        var names = [];
-        for (var i = 0; i < sockets.length; i++) {
-            names.push(sockets[i].userInfo.name);
-        }
-        return names;
-    }
-
-    function broadcast(message, socket) {
-        for (var i = 0; i < sockets.length; i++) {
-            var s = sockets[i];
-            if (s == socket) {
-                // not send to self
-                continue;
-            } else {
-                if (socket) {
-                    s.write("[" + socket.userInfo.name + "] " + message + "\r\n");
-                } else {
-                    s.write(message + "\r\n");
-                }
-            }
-        }
-    }
-
-    function addOneSocket(socket) {
-        broadcast("++ [" + socket.userInfo.name + "]" + " join the chat *");
-        sockets.push(socket);
-    }
-
-    function removeOneSocket(socket) {
-        var index = sockets.indexOf(socket);
-        if (index == -1) {
-            console.log("error - socket not found");
-            return;
-        }
-
-        var leftUserName = socket.userInfo.name;
-        sockets.splice(index, 1);
-        broadcast(leftUserName + " has left the room");
-    }
 });
 
 function UserInfo() {
-    var userName = null;
-
-    this.setUserName = function(ua) {
-        userName = ua;
+    var name = null;
+    this.setName = function(name){
+        this.name = name;
     }
-    this.getUserName = function() {
-        return ua;
+    this.getName = function(){
+        return this.name;
     }
 }
 
@@ -150,42 +48,133 @@ function Action(){
     this.setSocket = function(s){
         socket = s;
     }
-    
-    this.who = function(){
-        
+
+    this.welcome = function(){
+        var ipAddress = socket.remoteAddress;
+        socket.write("## welcom [" + ipAddress + "] and have a nice day ! ##\r\n");
+        socket.write("# type \\help to show help message\r\n");
+        socket.userInfo = new UserInfo();
+        socket.userInfo.setName(ipAddress);
+        sockets.push(socket);
     }
-    
+
+    this.leave = function(){
+        removeOneSocket();
+    }
+
     this.help = function(){
-        
+        socket.write("# \\who to check one line users\r\n");
+        socket.write("# \\change_name mike, to change your name to mike\r\n");
+        socket.write("# @mike xxx , to chat mike only\r\n");
+        socket.write("# \\help to show this list\r\n");
     }
-    
-    this.changeName = function(){
-        
+
+    this.who = function(){
+        var names = getUserNameList();
+        for (var i = 0; i < names.length; i++) {
+            socket.write("# " + names[i] + "\r\n");
+        }
     }
-    
-    this.sentMessage = function(){
-        
+
+    this.changeName = function(message){
+        var newName = message.replace("\\change_name ", "");
+        var oldName = socket.userInfo.getName();
+        socket.userInfo.setName(newName);
+        socket.write("# change your name to [" + newName + "]\r\n");
+        broadcast("# [" + oldName + "] change name to [" + newName + "]");
+    }
+
+    this.broadcast = function(message){
+        broadcast(message);
+    }
+
+    this.sendMessage = function(message){
+        var fromName = socket.userInfo.getName();
+        var privateMsg = message.split(" ")[1];
+        var toName = message.split(" ")[0].replace("@", "");
+        var toSocket = findSocketByName(toName);
+        if(!toSocket){
+            socket.write("# sorry , user not found .\r\n");
+        }else{
+            toSocket.write("* [" + fromName + "] say to you: " + privateMsg + "\r\n");
+        }
+    }
+
+    function broadcast(message) {
+        for (var i = 0; i < sockets.length; i++) {
+            var s = sockets[i];
+            if (s == socket) {
+                // not send to self
+                continue;
+            } else {
+                if (socket) {
+                    s.write("[" + socket.userInfo.getName() + "] " + message + "\r\n");
+                } else {
+                    s.write(message + "\r\n");
+                }
+            }
+        }
+    }
+
+    function addOneSocket() {
+        broadcast("# ++ [" + socket.userInfo.getName() + "]" + " join the chat #");
+        sockets.push(socket);
+    }
+
+    function removeOneSocket() {
+        var index = sockets.indexOf(socket);
+        if (index == -1) {
+            console.log("error - socket not found");
+            return;
+        }
+
+        var leftUserName = socket.userInfo.getName();
+        sockets.splice(index, 1);
+        broadcast(leftUserName + " has left the room");
+    }
+
+    function findSocketByName(name) {
+        for (var i = 0; i < sockets.length; i++) {
+            var s = sockets[i];
+            if (s.userInfo.getName() == name) {
+                return s;
+            }
+        }
+    }
+
+    function getUserNameList() {
+        var names = [];
+        for (var i = 0; i < sockets.length; i++) {
+            names.push(sockets[i].userInfo.getName());
+        }
+        return names;
     }
 }
 
 /**
- * socket请求的中央控制器，负责根据请求调用控制器中的各个方法
- * 提供给一个类方法dispatch
+ * socket请求的中央控制器，
+ * 负责根据请求调用控制器中的各个方法 提供给一个类方法dispatch
  */
 function Dispatcher(){}
-Dispatcher.dispatch = function(message){
+Dispatcher.dispatch = function(socket, message){
     if (/^\\who$/.test(message)) {
+        console.log("111111111");
         socket.action["who"](message);
+        return;
     }
     if (/^\\help$/.test(message)) {
         socket.action["help"](message);
+        return;
     }
     if (/^\\change_name\s.*$/.test(message)) {
         socket.action["changeName"](message);
+        return;
     }
     if (/^@\w+\s.*$/.test(message)) {
-        socket.action["sendMessagePrivate"](message);
+        socket.action["sendMessage"](message);
+        return;
     }
+    socket.action["broadcast"](message);
 }
 
 server.listen(1234, "127.0.0.1");
