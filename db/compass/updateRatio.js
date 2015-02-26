65e6dcf14dbc9c89a9e39ddf2eff6169ba224c5f
yisuren <zhao_hongsheng@microad-tech.com>
2015-02-26 14:39:29 +0800

    update updateRatio.js

--- db/compass/updateRatio.js
+++ db/compass/updateRatio.js
@@ -3,39 +3,46 @@ var mysql = require('mysql');
 var async = require("async");
 
 
-/*var connection = mysql.createConnection({
-  host     : '192.168.196.10',
-  port     : '9918',
-  database : "monoliths_master_db",
-  user     : 'd_signage',
-  password : 'signage'
-});*/
-
-  
 var conn = mysql.createConnection({
-  host     : 'localhost',
-  port     : '3306',
+  host     : '192.168.196.10',
+  port     : '4306',
   database : "compass_master_db",
-  user     : 'dev',
+  user     : 'root',
   password : 'password'
 });
+
 var connCore = mysql.createConnection({
-    host     : 'localhost',
-    port     : '3306',
+    host     : '192.168.196.10',
+    port     : '4306',
     database : "core_master_db",
-    user     : 'dev',
+    user     : 'root',
     password : 'password'
   });
 
+//var conn = mysql.createConnection({
+//  host     : 'localhost',
+//  port     : '3306',
+//  database : "compass_master_db",
+//  user     : 'dev',
+//  password : 'password'
+//});
+//var connCore = mysql.createConnection({
+//    host     : 'localhost',
+//    port     : '3306',
+//    database : "core_master_db",
+//    user     : 'dev',
+//    password : 'password'
+//});
+
 
 var params = {
-  structId: "81122",
-  startDate: "2015-02-25",
+  structId: "81191",
+  startDate: "2015-02-20",
   endDate: "2015-02-28",
   ratioFlg: true,
   ratios: [{
-      startDate: "2015-02-25",
-      endDate: "2015-02-25",
+      startDate: "2015-02-20",
+      endDate: "2015-02-28",
       deliveryRatio: "13.000",
       forecastCpm: "14.0000"
   },{
@@ -89,6 +96,76 @@ function insertReserveDeliveryRatio(next) {
     }
 }
 
+function selectReserveDeliveryRatio(next){
+    var sql = "select * from reserve_delivery_ratio where struct_id = ?";
+    conn.query(sql, params.structId, function(error, result){
+        if(!error){
+            console.log("-- select reserve_delivery_ratio ok, " + result.length);
+        }
+        next(error, result);
+    });
+}
+
+function updateReserveDeliveryRatio(result, next){
+    if(params.ratioFlg === false){
+        next();
+    }else{
+        var i = 0;
+        async.each(params.ratios, function(ratioItem, eachNext){
+            var resultItem = result[i];
+            i++;
+            if(!resultItem){
+                eachNext();
+                return;
+            }
+            var sql = "update reserve_delivery_ratio set start_date = ?, end_date = ? where ratio_sequence = ?";
+            conn.query(sql, [ratioItem.startDate, ratioItem.endDate, resultItem["ratio_sequence"]], function(error){
+                eachNext(error);
+            });
+        }, function(error){
+            if(!error){
+                console.log("-- update reserveDeliveryRatio ok");
+            }
+            next(error);
+        });
+    }
+}
+
+function selectReserveTotalTargetImpression(next){
+    var sql = "select * from reserve_total_target_impression where struct_id = ?";
+    conn.query(sql, [params.structId], function(error, result){
+       if(!error){
+           console.log("-- select reserve_total_target_impression ok ", result.length);
+       }
+       next(error, result);
+    });
+}
+
+function updateReserveTotalTargetImpression(result, next){
+    if(params.ratioFlg === false){
+        next();
+    }else{
+        var i = 0;
+        async.each(params.ratios, function(ratioItem, eachNext){
+            var resultItem = result[i];
+            i++;
+            if(!resultItem){
+                eachNext();
+                return;
+            }
+            var sql = "update reserve_total_target_impression set start_date = ?, end_date = ? where total_sequence = ?";
+            conn.query(sql, [ratioItem.startDate, ratioItem.endDate, resultItem["total_sequence"]], function(error){
+                eachNext(error);
+            });
+        }, function(error){
+            if(!error){
+                console.log("-- update reserve_total_target_impression ok");
+            }
+            next(error);
+        });
+    }
+}
+
 function beginTransaction(callback){
     conn.beginTransaction(function(err) {
         // start transaction one ...
@@ -108,17 +185,28 @@ function beginTransaction(callback){
 }
 
 function main(){
-    async.waterfall([ updateStruct, deleteAllRatio, insertReserveDeliveryRatio ], function(error) {
+    async.waterfall([ updateStruct,
+//                      deleteAllRatio,
+//                      insertReserveDeliveryRatio
+                      selectReserveTotalTargetImpression,
+                      updateReserveTotalTargetImpression,
+                      selectReserveDeliveryRatio,
+                      updateReserveDeliveryRatio
+                      ], function(error) {
         if (error) {
             console.log(error);
-            conn.rollback();
-            connCore.rollback();
-            process.exit();
+            conn.rollback(function(){
+                connCore.rollback(function(){
+                    process.exit();
+                });
+            });
         } else {
-            conn.commit();
-            connCore.commit();
+            conn.commit(function(){
+                connCore.commit(function(){
+                    process.exit();
+                });
+            });
             console.log("done ...");
-            process.exit();
         }
     });
 }
