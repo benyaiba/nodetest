0a2da39c265ec88cc6678192be09f48556a9d2ba
yisuren <zhao_hongsheng@microad-tech.com>
2015-06-10 17:07:38 +0800

    diaper m

--- diaper/diaper2.js
+++ diaper/diaper2.js
@@ -1,23 +1,39 @@
 var request = require('request');
 var harJson = require("./har.json");
 
-//console.log(harJson);
-//console.log(harJson.log.entries);
-function getRequestsFromHar(){
+// console.log(harJson);
+// console.log(harJson.log.entries);
+function getRequestsFromHar() {
     var entries = harJson.log.entries;
     var urlReg = /https:\/\/advs.*?cgi(?!&)/;
     var requests = [];
-    entries.forEach(function(entry){
+    entries.forEach(function(entry) {
         var request = entry.request;
         var url = request.url;
-//        console.log(url, urlReg.test(url));
-        if(urlReg.test(url)){
+        // console.log(url, urlReg.test(url));
+        if (urlReg.test(url)) {
             requests.push(request);
         }
     });
-//    requests.forEach(function(r){
-//        console.log(r.url);
-//    });
+    // requests.forEach(function(r){
+    // console.log(r.url);
+    // });
+    return requests;
 }
 
-getRequestsFromHar();
+var requests = getRequestsFromHar();
+var r = requests[0];
+
+request({
+    har: r
+}, function(err, response, body) {
+    if (err) {
+        cosole.log("err:", err);
+    }else{
+        if(body.indexOf("エラー") != -1){
+            console.log("e ....");
+        }else{
+            console.log("s ...");
+        }
+    }
+});
