5f37e5de1b6537efa4f6071fec19aa27c02d851d
zhao_hongsheng <zhao_hongsheng@microad-tech.com>
2015-10-22 15:19:53 +0800

    update calc.js

--- utils/test/calc.js
+++ utils/test/calc.js
@@ -8,9 +8,9 @@ function exhaustive(){
         "y": random2Arr(),
         "b": random2Arr(),
         "d": random(4)
-
     }
 
+    // condition 1
     var count = 0;
     var flag = false;
     for(var i =0;i<4;i++){
@@ -33,50 +33,39 @@ function exhaustive(){
         }
     }
     if(flag === false){
-        console.log("condition 1 NG");
         return;
     }
 
-
+    // conditon 2
     if(map.j.indexOf(1) == -1){
-        console.log("condition 2 NG");
         return;
     }
     if(map.d == 1){
-        console.log("condition 2 NG");
         return;
     }
     if(map.y.indexOf(0) != -1){
-        console.log("condition 2 NG");
         return;
     }
 
+    // condition 3
     if(map.b.indexOf(map.j[0]) != -1 || map.b.indexOf(map.j[1]) != -1){
-        console.log("condition 3 aaaaa NG");
         return;
     }
     if(map.y.indexOf(map.b[0]) == -1 && map.y.indexOf(map.b[1]) == -1){
-        console.log("condition 3 bbbbb  NG");
         return;
     }
     if(map.b.indexOf(map.d) != -1){
-        console.log("condition 3 cccccc NG");
         return;
     }
 
+    // condition 4
     if(map.j.indexOf(1) !=-1 && map.j.indexOf(3) !=-1){
-//        console.log(map);
-        console.log("condition 4 aaaa NG");
         return;
     }
     if(map.y.indexOf(1) !=-1 && map.y.indexOf(3) !=-1){
-//        console.log(map);
-        console.log("condition 4 bbbb NG");
         return ;
     }
     if(map.b.indexOf(1) !=-1 && map.b.indexOf(3) !=-1){
-//        console.log(map);
-        console.log("condition 4 ccccc NG");
         return ;
     }
 
